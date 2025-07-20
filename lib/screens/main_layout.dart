0eb608abf42e1665440321177bb58533298cec80
sathmini subasinghe <sathmini@leadinnovationz.com>
2025-07-20 14:46:20 +0530

    new changes

--- lib/screens/main_layout.dart
+++ lib/screens/main_layout.dart
@@ -660,7 +660,7 @@ void _onItemTapped(int index) {
             height: size,
             fit: BoxFit.cover,
             errorBuilder: (context, error, stackTrace) {
-              print('❌ Error displaying base64 image in navbar1: $error');
+              print('❌ Error displaying base64 image in navbar12: $error');
               return _buildDefaultAvatar();
             },
           ),
